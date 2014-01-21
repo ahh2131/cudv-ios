01fce3d385bd0d7358417db736c8535e89e66436
Andy <ahh2131@columbia.edu>
2014-01-21 18:37:26 -0500

    initial commit

---
+++ CUDiningView/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  CUDiningView
+//
+//  Created by Andy on 1/16/14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UITableViewController
+
+@end
---
+++ CUDiningView/NutritionViewController.h
@@ -0,0 +1,13 @@
+//
+//  NutritionViewController.h
+//  CUDiningView
+//
+//  Created by Andy on 1/16/14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NutritionViewController : UITableViewController
+
+@end
